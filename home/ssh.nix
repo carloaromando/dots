739aeeef7c16e4820e52150e7723082068a36513
carloaromando <carlo.aromando@gmail.com>
2023-11-07 16:03:21 +0100

    add gitignore

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,6 +3,11 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "gh-personal" = {
+        user = "git";
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_personal_ed25519";
+      };
       "yocto-vm" = {
         user = "yocto";
         hostname = "localhost";
@@ -20,16 +25,16 @@
           host.address = "cubbit-db.cfhojsg4cdow.eu-west-1.rds.amazonaws.com";
           host.port = 5432;
         }];
-        identityFile = "/Users/carlo/.ssh/id_rsa";
+        identityFile = "~/.ssh/id_rsa";
       };
       "cubbit-network" = {
         hostname = "network.cubbit.dev";
         user = "ubuntu";
-        identityFile = "/Users/carlo/.ssh/id_rsa";
+        identityFile = "~/.ssh/id_rsa";
       };
       "cubbit-coder" = {
         hostname = "coder.cubbit";
-        proxyCommand = "${lib.getExe pkgs.coder} --global-config \"/Users/carlo/Library/Application Support/coderv2\" ssh --stdio cubbit";
+        proxyCommand = "${lib.getExe pkgs.coder} --global-config \"~/Library/Application Support/coderv2\" ssh --stdio cubbit";
         extraOptions = {
           connectTimeout = "0";
           userKnownHostsFile = "/dev/null";
